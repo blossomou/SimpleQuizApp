b42ea321c48112f91f4ab1b5ea2c75330ea46867
QingOu <qingqingou@gmail.com>
2022-01-19 10:41:16 -0500

    initial set up

--- App.tsx
+++ App.tsx
@@ -1,10 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+
+import Questions from './components/Questions';
+import QUESTIONDATA from './data.json';
+
+const dbURL = "https://opentdb.com/api.php?amount=10&category=18&type=multiple";
+
+interface QuestionProps {
+  category: string;
+  type: string;
+  difficulty: string;
+  question: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+}
 
 export default function App() {
+  const [questions, setQuestions] = useState<QuestionProps[] | null>(null);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const data: QuestionProps[] = QUESTIONDATA.results;
+
+  useEffect(() => {
+    setQuestions(data);
+  }, []);
+
+  const getData = () => {
+    if (data.length > 0) {
+      data.map((question) => {
+        question.incorrect_answers.push(question.correct_answer);
+      });
+
+      setQuestions(data);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View>
+      <Pressable style={styles.button} onPress={getData}>
+        <Text style={styles.text}>Click Me</Text>
+      </Pressable>
+
+      {questions?.map((question, index) => {
+        return <Questions data={question} key={index} />;
+      })}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +52,59 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    height: "50%",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: "black",
+    borderStyle: "dotted",
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "black",
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
   },
 });
+
+// const onPressHandler = () => {
+//   fetch(dbURL)
+//     .then((response) => {
+//       if (response.ok) {
+//         response.json().then((data) => {
+//           if (data.results.length > 0) {
+//             console.log(data.results);
+//           }
+//         });
+//       }
+//     })
+//     .catch((err) => console.log(err));
+// };
+
+// const onPressHandlerAsync = async () => {
+//   try {
+//     const response = await fetch(dbURL);
+
+//     if (response.ok) {
+//       const data = await response.json();
+
+//       if (data.results.length > 0) {
+//         console.log(data.results);
+//       }
+//     }
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
